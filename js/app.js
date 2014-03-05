5893748fb86595159085ddf9ecbe1f5b4468388b
tiagosaraiva <plus.tiago@gmail.com>
2014-03-05 16:07:06 -0300

    Prototipo site itgroup

---
+++ js/app.js
@@ -0,0 +1,61 @@
+﻿
+
+
+$(document).ready(function () {
+
+
+
+    $('#slideshow').bjqs({
+        'animtype': 'slide',
+        'responsive': true,
+        'showcontrols': true,
+        'showmarkers': false,
+        'randomstart': false,
+        'nexttext': "<span id='btn-proximo'>proximo</span>",
+        'prevtext': "<span id='btn-anterior'>anterior</span>"
+
+    });
+
+    $("#img-esquerda").on('click', function () {
+
+        $("#btn-anterior").click();
+    });
+    $("#img-direita").on('click', function () {
+
+        $("#btn-proximo").click();
+    });
+
+    $("#esquerda-1").liveTile({ mode: 'flip', initDelay: 500 });
+    $("#esquerda-2").liveTile({ mode: 'flip', initDelay: 500 });
+    $("#esquerda-3").liveTile({ mode: 'flip', initDelay: 500 });
+    $("#esquerda-4").liveTile();
+
+    
+
+    $("#direita-1").liveTile({ mode: 'flip', initDelay: 500 });
+    $("#direita-2").liveTile({ mode: 'flip', initDelay: 500 });
+    $("#direita-3").liveTile({ mode: 'flip', initDelay: 500 });
+    $("#direita-4").liveTile({ mode: 'flip', initDelay: 500 });
+
+   
+
+    $("#flip1").liveTile(
+        {
+            initDelay: 500,
+            mode: 'flip-list',
+            swap: 'image',
+            frontIsRandom: true,
+            backIsBackgroundImage: false,
+            backIsInGrid: true,
+            backIsRandom: false,
+            // every tile in the list should flip every time the delay interval occurs
+            alwaysTrigger: true,
+            //flip the tiles in sequence
+            triggerDelay: function (idx) { return idx * 500; }
+        }
+    );
+
+
+
+
+})
