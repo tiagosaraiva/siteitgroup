d670b5aef4ca093cc84929d6783e0ab134768d25
tiagosaraiva <plus.tiago@gmail.com>
2014-03-17 13:05:05 -0300

    Desenho de novo layout (flat)

--- js/app.js
+++ js/app.js
@@ -4,59 +4,31 @@
 $(document).ready(function () {
 
 
+/*    $(function () {
 
-    $('#slideshow').bjqs({
-        'animtype': 'slide',
-        'responsive': true,
-        'showcontrols': true,
-        'showmarkers': false,
-        'randomstart': false,
-        'nexttext': "<span id='btn-proximo'>proximo</span>",
-        'prevtext': "<span id='btn-anterior'>anterior</span>"
-
-    });
-
-    $("#img-esquerda").on('click', function () {
-
-        $("#btn-anterior").click();
-    });
-    $("#img-direita").on('click', function () {
-
-        $("#btn-proximo").click();
-    });
-
-    $("#esquerda-1").liveTile({ mode: 'flip', initDelay: 500 });
-    $("#esquerda-2").liveTile();
-    $("#esquerda-3").liveTile({ mode: 'flip'});
-    $("#esquerda-4").liveTile();
-
-    
-
-    $("#direita-1").liveTile({ mode: 'flip'});
-    $("#direita-2").liveTile({ mode: 'slide'});
-    $("#direita-3").liveTile({ mode: 'flip', initDelay: 400 });
-    $("#direita-4").liveTile({ mode: 'flip', initDelay: 200 });
-    $("#direita-5").liveTile({ mode: 'slide', initDelay: 500 });
-    $("#direita-6").liveTile({ mode: 'slide', initDelay: 100 });
-
-   
-
-    $("#flip1").liveTile(
-        {
-            initDelay: 500,
-            mode: 'flip-list',
-            swap: 'image',
-            frontIsRandom: true,
-            backIsBackgroundImage: false,
-            backIsInGrid: true,
-            backIsRandom: false,
-            // every tile in the list should flip every time the delay interval occurs
-            alwaysTrigger: true,
-            //flip the tiles in sequence
-            triggerDelay: function (idx) { return idx * 500; }
+        
+           
+        var windowH = $(window).height();
+        var wrapperH = $('.wrapper').height();
+        if (windowH > wrapperH) {
+            $('.wrapper').css({ 'height': ($(window).height()) + 'px' });
         }
-    );
+        $(window).resize(function () {
+            var windowH = $(window).height();
+            var wrapperH = $('.wrapper').height();
+            var differenceH = windowH - wrapperH;
+            var newH = wrapperH + differenceH;
+            var truecontentH = $('.truecontent').height();
+            if (windowH > truecontentH) {
+                $('.wrapper').css('height', (newH) + 'px');
+            }
 
+        })
+    });*/
+
+    $('.navbar-nav').onePageNav({
+    
+    });
 
 
 
